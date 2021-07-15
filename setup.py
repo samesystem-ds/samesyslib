d02e56b2c9913253788313a97eabcb57563ab9e3
Giedrius Blazys <giedrius.blazys@samesystem.com>
2021-07-15 14:41:02 +0300

    increase version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="samesyslib",
-    version="0.0.18",
+    version="0.0.19",
     author="SameSystem",
     author_email="giedrius.blazys@samesystem.com",
     description="Common libs used by SameSystem Data Science team.",
