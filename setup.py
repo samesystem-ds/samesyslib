04d51863e9c769591e10dab9e9c8219f9b1801c4
linas-samesystem <50135698+linas-samesystem@users.noreply.github.com>
2022-01-26 08:51:28 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="samesyslib",
-    version="0.0.28",
+    version="0.0.29",
     author="SameSystem",
     author_email="giedrius.blazys@samesystem.com",
     description="Common libs used by SameSystem Data Science team.",
